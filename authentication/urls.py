1a8e62fc0e57cdf079a58a4b5a986773ab87a652
Narawish <narawish_k@kkumail.com>
2024-10-23 21:09:51 +0700

    Test branching

--- authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-# from .views import CustomTokenPairView
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-
-
-urlpatterns = [
-    path('token/', TokenObtainPairView.as_view())
-]
